666ad7b2e396248235f23c898b7ceae46124a5d2
Praisg <gavipraise@gmai.com>
2023-02-19 03:03:59 -0800

    hamemo

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- bet
+ * Return: 0
+ */
+
+int main(void)
+{
+char c = 'z';
+
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
