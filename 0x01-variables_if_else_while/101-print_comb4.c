9fa1c1f0467520e1c3ef16ebf1f1aa9078b7ba24
Praisg <gavipraise@gmai.com>
2023-02-19 05:33:19 -0800

    3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - 3 combinations
+ * Return: 0
+ */
+
+int main(void)
+{
+int x = '0';
+int y = '0';
+int z = '0';
+
+for (z  = '0'; z <= '0'; z++)
+{
+for (y = '0'; y <= '0'; y++)
+{
+for (x = '0'; x <= '0'; x++)
+{
+if (!((x == y) || (y == z) || (y > x) || (z > y)))
+{
+putchar(z);
+putchar(y);
+putchar(x);
+if (!(x == 'o' && z == '7' && x == '8'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
