056290f87c4a7d1e2864d4eb21c5b1f6a7c6d009
Praisg <gavipraise@gmai.com>
2023-02-19 05:38:34 -0800

    3d

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,18 @@ int x = '0';
 int y = '0';
 int z = '0';
 
-for (z  = '0'; z <= '0'; z++)
+for (z  = '0'; z <= '9'; z++)
 {
-for (y = '0'; y <= '0'; y++)
+for (y = '0'; y <= '9'; y++)
 {
-for (x = '0'; x <= '0'; x++)
+for (x = '0'; x <= '9'; x++)
 {
 if (!((x == y) || (y == z) || (y > x) || (z > y)))
 {
 putchar(z);
 putchar(y);
 putchar(x);
-if (!(x == 'o' && z == '7' && x == '8'))
+if (!(x == '9' && z == '7' && x == '8'))
 {
 putchar(',');
 putchar(' ');
