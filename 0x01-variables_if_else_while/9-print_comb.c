04cd49932a2d44e81d119fd2527d60f35fc438c3
Praisg <gavipraise@gmai.com>
2023-02-19 04:43:11 -0800

    single d

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - all single num
+ * Return: 0
+ */
+
+int main(void)
+{
+int c = 0;
+
+while (c < 10)
+{
+putchar(48 + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+}
