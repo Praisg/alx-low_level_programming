b7ab381a62114834a1cf0cdfea24808f94bd53f4
Praisg <gavipraise@gmai.com>
2023-02-24 00:51:49 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar -writes the character
+ * @c: character to be printed
+ * Return: On sucess 1 on error -1
+ */
+
+int _putchar(char c)
+{
+return (write(1, &C, 1));
+}
