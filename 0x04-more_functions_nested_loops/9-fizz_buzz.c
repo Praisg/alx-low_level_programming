0258ba112b49d3cbb884491285f793f24140e4c3
Praisg <gavipraise@gmai.com>
2023-02-24 05:59:51 -0800

    o

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,28 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz\n");
+printf("Fizz");
 }
-else if (i % 5 == 0)
+else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Buzz\n");
+printf("Buzz");
 }
 else if (i % 5 == 0 && i % 3 == 0)
 {
-printf("FizzBuzz\n");
+printf("FizzBuzz");
 }
-}
+else if (i == 1)
 {
-printf("%d\n", i);
+printf("%d", i);
 }
+else
+{
+printf("%d", i);
+}
+}
+printf("\n");
+
 return (0);
 }
