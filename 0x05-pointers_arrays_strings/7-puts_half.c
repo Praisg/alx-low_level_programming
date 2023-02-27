e0f2d367b3ba74a4c1046d49b057f25fb2afd297
Praisg <gavipraise@gmai.com>
2023-02-27 15:38:21 -0800

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,24 @@
 #include "main.h"
 
 /**
- * _strlen
+ * puts_half - prinys half
+ * @str: input
+ */
+
+void puts_half(char *str)
+{
+int len = 0;
+
+while (*str != '\0')
+{
+len++;
+str++;
+}
+str -= (len / 2);
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+}
+putchar('\n');
+}
