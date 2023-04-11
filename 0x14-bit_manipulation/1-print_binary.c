2f87526255a508adcc2bc1ae05fc46c6ed1ea58d
Praisg <gavipraise@gmail.com>
2023-04-10 18:33:36 -0700

    bin repre

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+int j;
+int val = 0;
+unsigned long int org;
+
+for (j = 1 << 31; j > 0; j >>= 1)
+{
+org = n >> j;
+
+if (org & 1)
+{
+_putchar('1');
+val++;
+}
+else if (val)
+_putchar ('0');
+}
+if (val == NULL)
+_putchar ('0');
+}
